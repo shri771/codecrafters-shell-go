3ce6289d0ce23c58b528547e45e2a8cdd2169516
Shrikant Shingare <shrikantshingare77@gmail.com>
2026-06-16 15:08:18 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -26,7 +26,8 @@ func main() {
 		line := reader.Text()
 
 		// Sanitize Args
-		parts := strings.Fields(strings.ToLower(line))
+		// parts := strings.Fields(strings.ToLower(line))
+		parts := strings.Fields(line)
 
 		if len(parts) == 0 {
 			continue
